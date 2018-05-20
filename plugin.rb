004a52d84d86422d91420b24b1cdb44b4dda0a52
Joshua Rosenfeld <jomaxro@discourse.org>
2018-05-19 21:26:28 -0400

    Update name and URL

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,7 @@
-# name: BBCode
+# name: discourse-bbcode
 # about: adds BBCode to Discourse
 # version: 0.1
 # authors: Régis Hanol, Sam Saffron
+# url: https://github.com/discourse/discourse-bbcode
 
 register_asset "stylesheets/bbcode.scss"
